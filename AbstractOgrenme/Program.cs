158ca0ffe184a6b4180e1e56a83d2185f6c6e001
salih <salih_yil@live.com>
2021-02-28 18:35:58 +0300

    .

---
+++ AbstractOgrenme/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace AbstractOgrenme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Abstract classlar hem tamamlanmış hemde tamamlanmamış operasyon içerebilen classdır.
+
+            //MevzuatBase mevzuatBase = new MevzuatBase(); Absract classlar newlenemez.
+            AMevzuat aMevzuat = new AMevzuat();
+            aMevzuat.Kaydet(); // Kaydet() abstractın içinde ama isteğe bağlı kullanılabiliyor.  
+
+        }
+
+        abstract class MevzuatBase
+        {
+            public abstract void Degerlendirme(); //Bu tamamlanmamış operasyon
+            public void Kaydet() 
+            {
+                Console.WriteLine("Kaydedildi.");
+            }
+        }
+
+        class AMevzuat : MevzuatBase
+        {
+            public override void Degerlendirme()
+            {
+                Console.WriteLine();
+            }
+        }
+        
+
+
+
+        
+        ///////////////////////////////////////////////////////////////////////////////////
+        class OrnekBase
+        {
+            //void ddd(); normal classlarda tamamlanmamış operasyon olmaz. 
+            public void Naber() 
+            { 
+                Console.WriteLine("Yazdır");  
+            }
+            public string ozellik { get; set; }
+        }
+        
+        class Ornek1: OrnekBase // normal class ile normal classda digerinin OrnekBase içindeki metot kullanılması zorunlu değil.
+        {
+            public int MyProperty { get; set; }
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+
+
+    }
+}
---
+++ InheritanceAndInterface/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace InheritanceAndInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPersonManager customerPerson = new CustomerManager();
+            customerPerson.Add(); // Müsteri eklendi. yazacak.
+
+            IPersonManager employeePerson = new EmployeeManager();
+            employeePerson.Add(); // Employee eklendi. yazacak.
+
+            Console.WriteLine("------------------------------------------------");
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(customerPerson); //Müsteri eklendi. yazacak.
+            projectManager.Add(employeePerson); //Müsteri eklendi. yazacak.
+            projectManager.Update(customerPerson);//Müsteri güncellendi. yazacak.
+            projectManager.Add(new InternManager()); // Stajyer eklendi. yazacak.
+        }
+
+        interface IPersonManager
+        {
+            void Add();
+            void Update();
+        }
+        class CustomerManager : IPersonManager
+        {
+            public void Add()
+            {
+                Console.WriteLine("Müsteri eklendi.");
+            }
+
+            public void Update()
+            {
+                Console.WriteLine("Musteri güncellendi.");
+            }
+        }
+        class EmployeeManager : IPersonManager
+        {
+            public void Add()
+            {
+                Console.WriteLine("Employee eklendi.");
+            }
+
+            public void Update()
+            {
+                Console.WriteLine("Employee güncellendi.");
+            }
+        }
+        // Diyelim yeni talep geldi. Stajyer öğrencininde eklenmesi ve güncellenmesi olsun istenirse 
+        class InternManager : IPersonManager
+        {
+            public void Add()
+            {
+                Console.WriteLine("Stajyer eklendi.");
+            }
+
+            public void Update()
+            {
+                Console.WriteLine("Stajyer güncellendi.");
+            }
+        }
+
+        class ProjectManager //tüm projeyi tek noktadan yönetmek istesek. 
+        {
+
+            //Her zaman bir nesne ile değil birden fazla nesne ile çalışırsın.
+            public void Add(IPersonManager personManager)  //IPerson customerPerson = new CustomerManager(); Bunu yazma sebebimiz burda IPerson ile customerPerson ile EmployeePersona ulaşıyoruz. 
+            {
+                personManager.Add();           
+            }
+            public void Update(IPersonManager personManager)  //IPerson customerPerson = new CustomerManager(); Bunu yazma sebebimiz burda IPerson ile customerPerson ile EmployeePersona ulaşıyoruz. 
+            {
+                personManager.Update();
+            }
+        }
+
+        
+    }
+    
+}
